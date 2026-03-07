b325178b50cf0f361be6e86adce70246e68eccbb
Tisha Dey <tishadey2025@outlook.com>
2026-03-07 18:57:20 +0600

    :green_heart: sign in done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // alert("js file conected");
+document.getElementById("sign-btn").addEventListener("click", function(event){
+    // console.log(event);
+event.preventDefault();
+const username = document.getElementById("user-name").value;
+// console.log(username);
+const password = document.getElementById("password").value;
+// console.log(username,password);
+if(username==="admin"){
+    if(password ==="admin123" ){
+    window.location.href = "./main.html"
+    }else{
+    console.log("password tik nai");
+    }
+}else{
+   console.log("wrong username");
+}
+})
