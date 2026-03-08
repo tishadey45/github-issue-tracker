e8b6cfad9b6d168544ec3d6751b6c037b7717974
Tisha Dey <tishadey2025@outlook.com>
2026-03-08 12:24:19 +0600

    :hammer: card design issue fix

--- main.js
+++ main.js
@@ -9,18 +9,27 @@ const displayIssue = (issues) => {
   issueContainer.innerHTML = "";
   for (let issue of issues) {
     const element = document.createElement("div");
+    const colors = ["btn-error", "btn-warning", "btn-info", "btn-success"];
     element.innerHTML = `
-        <div class="card w-96 bg-base-100 shadow-sm">
+       
+
+
+        <div class="card bg-base-100 shadow-sm">
                     <div class="card-body bg-white rounded-xl shadow-sm  px-5 space-y-4">
                         <div class="flex justify-between">
                             <img src="./assets/Open-Status.png" alt="">
                             <button class="btn btn-soft btn-error rounded-4xl">${issue.priority}</button>
                         </div>
-                        <h2 class="font-semibold">${issue.title}</h2>
-                        <p class="text-gray-500">${issue.description}</p>
+                        <h2 class="font-semibold line-clamp-1">${issue.title}</h2>
+                        <p class="text-gray-500 line-clamp-2">${issue.description}</p>
                         <div class="flex gap-4">
-                            <button class="btn btn-soft btn-error rounded-4xl">labels</button>
-                            <button class="btn btn-soft btn-warning rounded-4xl ">labels</button>
+                      ${issue.labels
+                        .map(
+                          (label, index) =>
+                            `<button class="btn btn-soft ${colors[index % colors.length]} rounded-4xl">${label}</button>`,
+                        )
+                        .join("")}
+                           
                         </div>
                         <hr class="border border-gray-300">
                         <p class="text-gray-500">#1 by john_doe</p>
