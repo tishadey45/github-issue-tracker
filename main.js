0d2ecf2d74fbe57c85ac48d98e23a68b865a5ec7
Tisha Dey <tishadey2025@outlook.com>
2026-03-08 20:23:28 +0600

    :tada: dynamic card details

--- main.js
+++ main.js
@@ -3,6 +3,47 @@ const loadIssues = () => {
     .then((res) => res.json())
     .then((json) => displayIssue(json.data));
 };
+
+const loadIssueDetails = async (id) => {
+  // console.log("clicked");
+  const url = `
+    https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+  const res = await fetch(url);
+  const details = await res.json();
+  displayIssueDetails(details.data);
+};
+const displayIssueDetails = (issue) => {
+  // console.log(issue);
+  const issueContent = document.getElementById("modal-content");
+  
+  issueContent.innerHTML = `
+                    <div>
+                          <h3 class="text-lg font-bold">${issue.title}</h3>
+                          <div class="flex gap-2" >
+                          <span class="badge badge-success" >${issue.status}</span>
+                          <p class="text-sm text-gray-600">
+                         Opened by ${issue.author} • ${issue.createdAt}
+                        </p>
+                          </div>
+                         
+                        <p class="py-4 text-gray-600 ">${issue.description}</p>
+                        <div class="flex gap-25 bg-gray-100 py-4" >
+                        <div class="px-4" >
+                         <p class="text-gray-500" >assignee:</p>
+                        <span>${issue.assignee}</span>
+                        </div>
+                        <div>
+                        <p class="text-gray-500" >priority:</p>
+                        <span class="badge badge-error" >${issue.priority}</span>
+                        </div>
+                       
+                        </div>
+                        
+                        </div>
+`;
+  document.getElementById("issue_modal").showModal();
+};
+
 const displayIssue = (issues) => {
   // console.log(issues);
   const issueContainer = document.getElementById("issue-container");
@@ -14,7 +55,7 @@ const displayIssue = (issues) => {
        
 
 
-        <div class="card bg-base-100 shadow-sm">
+        <div class="card bg-base-100 shadow-sm" onClick ="loadIssueDetails(${issue.id})">
                     <div class="card-body bg-white rounded-xl shadow-sm  px-5 space-y-4">
                         <div class="flex justify-between">
                             <img src="./assets/Open-Status.png" alt="">
